4ecfe63a7f37909a30dfc395185eed1f7d3d8c48
Hadi Soofi <hadisoofi@gmail.com>
2019-02-02 10:15:46 +0330

    Added Point class

---
+++ src/Point.java
@@ -0,0 +1,32 @@
+import java.lang.Math;
+
+
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // rotate point around another point by an angle of alpha
+    public Point rotate(Point center, double alpha) {
+
+        Point p = this.subtract(center);
+        double xNew = p.x * Math.cos(alpha * Math.PI / 180.0) - p.y * Math.sin(alpha * Math.PI / 180.0) ;
+        double yNew = p.x * Math.sin(alpha * Math.PI / 180.0) + p.y * Math.cos(alpha * Math.PI / 180.0) ;
+        xNew += center.x;
+        yNew += center.y;
+        return new Point(xNew, yNew);
+    }
+
+    public Point add(Point p) {
+        return new Point(this.x + p.x, this.y + p.y);
+    }
+
+    public Point subtract(Point p) {return new Point(this.x - p.x, this.y - p.y);}
+
+
+}
